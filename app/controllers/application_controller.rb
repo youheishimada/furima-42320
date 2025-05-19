cf8a30b89db6b37e05e13cd4b1be095ad7df8b5f
youheishimada <dainoji11@gmail.com>
2025-05-19 17:14:03 +0900

    commi
    ログアウトからトップページに遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
  before_action :basic_auth
  before_action :configure_permitted_parameters, if: :devise_controller?
 
+   def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
  private
 
   def basic_auth
