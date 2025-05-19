deceae9c7d7b200f82de02f87013f08a54d7633d
youheishimada <dainoji11@gmail.com>
2025-05-19 14:54:25 +0900

    新規登録/ユーザー情報　バリデーション記述
    ニックネームが必須であること。
    
    メールアドレスが必須であること。
    
    メールアドレスが一意性であること。
    
    メールアドレスは、@を含む必要があること。
    
    パスワードが必須であること。
    
    パスワードは、6文字以上での入力が必須であること
    
    パスワードは、半角英数字混合での入力が必須であること
    
    パスワードとパスワード（確認）は、値の一致が必須であること

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :email, presence: true, uniqueness: true
+         validates :password, format: { 
+                    with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, 
+                    message: 'は英字と数字の両方を含めて設定してください' 
+                                       }   
+         
+        
+
 end
